74a67fa33cfc5c33c90848fb2551ca58f0d5bf07
Ben Cyna <benjamincyna@gmail.com>
2022-05-29 17:20:06 +0930

    added imports and comments

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+
+#open site
+
+#get item distance
+
+#if at distance x, jump
